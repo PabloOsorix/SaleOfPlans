0c67163be3dd5005efba5fd55133799f558e0e5c
PabloOsorix <osorixjuan@gmail.com>
2024-11-21 19:44:58 -0500

    👽️ Update absolute links for relative

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/SaleOfPlans/'
+    base: process.env.NODE_ENV === 'production' ? '/SaleOfPlans/' : '/'
 });
