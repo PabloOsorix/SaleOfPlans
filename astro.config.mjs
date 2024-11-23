f33bcae177394dc5a4e12fc64067b470602c3fca
PabloOsorix <osorixjuan@gmail.com>
2024-11-22 20:59:11 -0500

    ➕ Trying adding splide to vide in defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 
-
+import compress from 'astro-compress';
 
 
 // https://astro.build/config
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/SaleOfPlans/' : '/',
+  integrations: [compress()],
+  vite: {
+    ssr: {
+      external: ['@splidejs/splide'],
+    },
+  },
 });
