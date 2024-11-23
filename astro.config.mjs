c166c9d62b36cdf2a56469d005f2f2ecdb776e57
PabloOsorix <osorixjuan@gmail.com>
2024-11-22 20:47:23 -0500

    ➕ Add astro-minify dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+
+import compress from 'astro-compress';
+
+
 // https://astro.build/config
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/SaleOfPlans/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/SaleOfPlans/' : '/',
+  integrations: [compress()],
 });
